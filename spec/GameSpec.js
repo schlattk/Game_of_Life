6b3679e18fb538a56eb5fb6f26c40ed114c18071
konrad schlatte <konradschlatte@konrads-MacBook-Air.local>
2017-12-11 10:30:30 +0000

    included random population of the game grid
    all tests are passing

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -8,6 +8,16 @@ describe('Game', function(){
     expect(game.grid.length).toBe(10);
     expect(game.grid[9].length).toBe(5);
     expect(game.grid[0].length).toBe(5);
+  });
+  it('has a function that seeds the grid', function(){
+    game = new Game(10,5);
+    expect(game.seed).toBeDefined();
+  });
+  it('populates the grid with a random number of "X" and ""', function(){
+    game = new Game(10,5);
+    game.seed(0.5);
+    var flat_array = game.grid.reduce((a, b) => a.concat(b), []);
+    expect(flat_array).toContain('X');
+    expect(flat_array).toContain('');
   })
-
 });
--- src/game.js
+++ src/game.js
@@ -3,8 +3,9 @@
 var Game = function(x,y){
   this.grid = new Array(x);
   for(let i = 0; i < this.grid.length; i ++){this.grid[i] = new Array(y)};
-}
+};
 
-
-
-// for(let i = 0; i<x.length; i++){for (let j = 0; i<x.length; i++){x[i][j] = Math.random > 0.5 "X" : ""}};
+Game.prototype.seed = function(number){
+  for(let i = 0; i < this.grid.length; i ++)
+  {for(let j = 0; j < this.grid[i].length; j++){this.grid[i][j] = Math.random() > number ? "X" : "";}};
+};
