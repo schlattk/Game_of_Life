520feb19b91cf230f2f72ef81418623e9cf4e51b
konrad schlatte <konradschlatte@konrads-MacBook-Air.local>
2018-01-09 20:14:11 +0000

    simplified grid generation by using array fill method

--- src/game.js
+++ src/game.js
@@ -2,10 +2,11 @@
 
 var Game = function(x,y){
   this.grid = new Array(x);
-  for(let i = 0; i < this.grid.length; i ++){this.grid[i] = new Array(y)};
+  this.grid.fill(new Array(y));
+  // for(let i = 0; i < this.grid.length; i ++){this.grid[i] = new Array(y)};
 };
 
-Game.prototype.seed = function(number,){
+Game.prototype.seed = function(number){
   for(let i = 0; i < this.grid.length; i++)
     {for(let j = 0; j < this.grid[i].length; j++){
       this.grid[i][j] = Math.random() > number ? "X" : "";}
