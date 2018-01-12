5cd29d9077c5185035687f5e69f2437ec8a6e1df
konrad schlatte <konradschlatte@konrads-MacBook-Air.local>
2018-01-12 17:22:49 +0000

    simplified move method using two grids

--- src/game.js
+++ src/game.js
@@ -1,30 +1,35 @@
 'use strict';
 
 var Game = function(x,y){
-  this.grid = new Array(x).fill(new Array(y));
+  this.grid = new Array(x);
+  for(let i = 0; i < this.grid.length; i ++){this.grid[i] = new Array(y)};
+
 };
 
 Game.prototype.seed = function(number){
   this.grid.forEach((item) => {for(let j = 0; j < item.length; j++){
       item[j] = Randomizer.random(number);}
   });
+  console.log(this.grid);
 };
 
 Game.prototype.move = function(){
-  var counters = [];
-  var arr = [];
-  var new_grid = [];
-  this.grid.forEach((item,i) => {for(let j = 0; j < item.length; j++){
-                                            counters.push(Scanner.scan(this.grid,i,j));
-                                            arr.push(item[j]);}
-                      });
-  counters.forEach((item,i) => {if(arr[i] === "X"){item < 2 || item > 3 ? arr[i] = "" : arr[i] = "X"}
-                                    else {item === 3 ? arr[i] = "X" : arr[i] = ""}
+  var length = this.grid.length;
+  var counter_grid = new Array(length);
+
+  for(let i = 0; i < length; i ++){counter_grid[i] = new Array(length)};
+  counter_grid.forEach((item) => {for(let j = 0; j < length; j++){item[j] = "";}
                             });
 
-  while (arr.length > 0) {new_grid.push(arr.splice(0, this.grid.length));}
-  this.grid = new_grid;
-};
+  counter_grid.forEach((item,i) => {for(let j = 0; j < length; j++){
+                                            counter_grid[i][j] = Scanner.scan(this.grid,i,j);
+                                          }});
+
+  this.grid.forEach((item, i) => {for(let j = 0; j < length; j++){
+                                            if(this.grid[i][j] === "X"){counter_grid[i][j] < 2 || counter_grid[i][j] > 3 ? this.grid[i][j] = "" : this.grid[i][j] = "X"}
+                                            else {counter_grid[i][j] === 3 ? this.grid[i][j] = "X" : this.grid[i][j] = ""}
+                                          }});
+  };
 
 Game.prototype.print = function(){
   Printer.print(this.grid);
